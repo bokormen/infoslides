b3ee4663436bd85dfb3074c1b8d2d9660c48ed28
kryel <mc@kryel.com>
2014-06-12 22:31:45 +0200

    Added loop for sending slides at regular intervals

--- java/infoslides/src/main/java/no/teknikerlauget/infoslides/server/Sender.java
+++ java/infoslides/src/main/java/no/teknikerlauget/infoslides/server/Sender.java
@@ -12,6 +12,7 @@ import java.io.File;
  */
 public class Sender {
 	private DatabaseConnector databaseConnector;
+	private long sleepTime = 10000;
 
 	/**
 	 * Construct a new sender
@@ -19,6 +20,37 @@ public class Sender {
 	public Sender(File databaseSettingsPath) {
 		this.databaseConnector = new DatabaseConnector(databaseSettingsPath);
 //		databaseConnector.open();
+		startSending();
+	}
+
+	/**
+	 * Start sending slides to clients
+	 */
+	private void startSending() {
+		try {
+			sendLoop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();  // TODO
+		}
+	}
+
+	/**
+	 * Send slides at regular intervals
+	 *
+	 * @throws InterruptedException
+	 */
+	private void sendLoop() throws InterruptedException {
+		while (true) {
+			sendSlide();
+			Thread.sleep(sleepTime);
+		}
+	}
+
+	/**
+	 * Send a slide to the clients
+	 */
+	private void sendSlide() {
+		// TODO
 	}
 
 }
