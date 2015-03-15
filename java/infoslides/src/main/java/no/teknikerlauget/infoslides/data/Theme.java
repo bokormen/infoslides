d1c347d489cfb1c271b436782623c8bb25a73cc3
kryel <mc@kryel.com>
2015-03-15 16:50:33 +0100

    java - added: Theme

---
+++ java/infoslides/src/main/java/no/teknikerlauget/infoslides/data/Theme.java
@@ -0,0 +1,36 @@
+package no.teknikerlauget.infoslides.data;
+
+import org.json.simple.JSONObject;
+
+public class Theme {
+
+	private final String name;
+	private final String css;
+	private final String description;
+
+	public Theme(String name, String css, String description) {
+		this.name = name;
+		this.css = css;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCss() {
+		return css;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public JSONObject toJson() {
+		JSONObject object = new JSONObject();
+		object.put("name", name);
+		object.put("css", css);
+		object.put("description", description);
+		return object;
+	}
+}
