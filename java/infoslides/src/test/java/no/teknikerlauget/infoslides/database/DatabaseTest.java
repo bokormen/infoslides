2706463b1a370ab6669a222dd38d30b4543e20a2
Øyvind <bokormen-05@hotmail.com>
2015-03-22 13:27:06 +0100

    litt finpuss på koden

--- java/infoslides/src/test/java/no/teknikerlauget/infoslides/database/DatabaseTest.java
+++ java/infoslides/src/test/java/no/teknikerlauget/infoslides/database/DatabaseTest.java
@@ -1,6 +1,8 @@
 package no.teknikerlauget.infoslides.database;
 
 import no.teknikerlauget.infoslides.data.Tag;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import no.teknikerlauget.infoslides.Program;
@@ -12,9 +14,20 @@ import java.util.List;
  */
 public class DatabaseTest {
 
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
 	@Test
 	public void WriteTag() {
 		TestData testData = new TestData();
+        
 		List<Tag> tags = testData.GetNewTags();
 		DatabaseQueries databaseQueries = new DatabaseQueries(Program.databaseSettingsPath);
 
