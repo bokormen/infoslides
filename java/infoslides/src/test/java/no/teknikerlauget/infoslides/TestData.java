b922cc067a76a039ed24f5261061910c59c7fb6f
Øyvind <bokormen-05@hotmail.com>
2015-04-18 01:41:13 +0200

    fixing indent

--- java/infoslides/src/test/java/no/teknikerlauget/infoslides/TestData.java
+++ java/infoslides/src/test/java/no/teknikerlauget/infoslides/TestData.java
@@ -43,7 +43,7 @@ public class TestData {
 	public List<Day> getNewDays() {
 		List<Day> days = new ArrayList<>();
 		for (int i = 0; i < 7; i++) {
-            String startTime = String.format("%02d", i) + ":" + String.format("%02d", i);
+			String startTime = String.format("%02d", i) + ":" + String.format("%02d", i);
 			String endTime = String.format("%02d", i+10) + ":" + String.format("%02d", i+10);
 			Day day = new Day(i, startTime, endTime);
 
